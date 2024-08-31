de4195ee637b1914ee1e54719e0b6de8372b4878
Alec Romel Espino <espino.alec@gmail.com>
2024-08-31 02:17:14 -0400

    Learned how to properly Query GraphQL using python

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ key = os.getenv('smashgg_api')
 api_url = 'https://api.start.gg/gql/alpha'
 
 slugs = "tournament/genesis-9-1/event/ultimate-singles"
+phase_id = 1749308
 vars = {"slug": slugs}
+vars2 = {"phaseId": phase_id, "page": 1, "perPage": 3}
 header = {"Authorization": "Bearer " + key}
 
 
@@ -20,13 +22,49 @@ query getEventId($slug: String) {
   event(slug: $slug) {
     id
     name
-    phaseGroups{
-        phaseGroup
+    phases{
+        id
+        name
+    }
+    videogame{
+        stages{
+            name
+        }
     }
   }
 },
 
 '''
+
+query2 = '''
+query PhaseSets($phaseId: ID!, $page: Int!, $perPage: Int!) {
+  phase(id: $phaseId) {
+    id
+    name
+    sets(
+      page: $page
+      perPage: $perPage
+      sortType: STANDARD
+    ){
+      pageInfo {
+        total
+      }
+      nodes {
+        id
+        slots {
+          id
+          entrant {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+},
+
+'''
+
 payload = {'query': query, 'variables': vars}
 
 response = requests.post(url=api_url,json=payload,headers=header)
