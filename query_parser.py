441a612de06fa0827a8b2f931534bbc96f284b1a
Alec Romel Espino <espino.alec@gmail.com>
2024-09-03 02:11:02 -0400

    created get_scoreboard and constants.py

--- query_parser.py
+++ query_parser.py
@@ -62,5 +62,22 @@ def is_phase_complete(response):
     print(response.text)
     return response.status_code
 
+def player_parse(response):
+  if response.status_code == 200:
+    response_json = response.json()
+    data = response_json.get('data')
+    player = data.get('player')
+    user = player.get('user')
+    # print(json.dumps(data,indent=2))
+    set_json = json.dumps(user)
+    set_data = json.loads(set_json)
+
+    return set_data
+  else:
+    print(f"Query failed with status code {response.status_code}")
+    print(response.text)
+    return response.status_code
+  
+
   
   
